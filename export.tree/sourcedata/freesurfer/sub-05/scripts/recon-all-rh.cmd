99e5181499848f6ee11c898252176effe3c64392
jbwexler <jbwexler@tutanota.com>
2022-07-01 17:04:37 -0500

    graft cleanup

--- export.tree/sourcedata/freesurfer/sub-05/scripts/recon-all-rh.cmd
+++
@@ -1,1 +0,0 @@
-../../../../.git/annex/objects/XM/3v/MD5E-s13457--e20e21c940b054b0f81b5d52b2159d8f.cmd/MD5E-s13457--e20e21c940b054b0f81b5d52b2159d8f.cmd
--- export.tree/sourcedata/freesurfer/sub-11/scripts/recon-all-lh.cmd
+++
@@ -1,1 +0,0 @@
-../../../../.git/annex/objects/Zz/jg/MD5E-s13457--7e6c4ad638298a25d3a7246a63d24895.cmd/MD5E-s13457--7e6c4ad638298a25d3a7246a63d24895.cmd
